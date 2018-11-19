35dd0e410d454f9c9bee9eda6ac5e625492a745d
bas.pieren@quicknet.nl <bas.pieren@quicknet.nl>
2018-11-19 13:52:22 +0100

    index.js | Edit | Write data to data.json

--- js/index.js
+++ js/index.js
@@ -1,82 +1,76 @@
 /* ------ SOURCES
 https://github.com/rijkvanzanten/node-oba-api
-https://stackoverflow.com/questions/16470113/how-to-return-part-of-string-after-a-certain-character
-Martijn Reeuwijk
+Laurens Aarnoudse
 Jesse Dijkman
-Laurens
+Sterre van Geest
 ------ */
 
 require('dotenv').config()
 
+var fs = require('file-system')
+
 // START USE OF SOURCE: https://github.com/rijkvanzanten/node-oba-api
 const OBA = require('oba-api')
 
-// Setup authentication to api server
 const client = new OBA({
-  // ProQuest API Keys
   public: process.env.PUBLIC,
   secret: process.env.SECRET
 })
 
-// General usage:
-// client.get({ENDPOINT}, {PARAMS});
-// ENDPOINT = search | details | refine | schema | availability | holdings
-// PARAMS = API url parameter options (see api docs for more info)
-
-// Client returns a promise which resolves the APIs output in JSON
-
-// Example search to the word 'rijk' sorted by title:
 client
   .get('search', {
-    q: 'Wereldoorlog',
+    rctx: 'AWNkYOZmYGcwrEorS801zTXOLSvMNEyqMEoqN6wyzkpOZWZk4MxNzMxjZGYQT8svyk0ssUrKz8@mBBGMzNKZ8UWpycUFqUUFiemprEYGTAwXQm4Z3DJgalvEyKixTIJ5gwUDA3t$UiIDA2dlamKRon5Rfn6Jfk5mYWlmij5QnL20KIeBNS$HEQA',
+    q: 'format:book',
     refine: true,
     sort: 'year',
-    facet: [
-      'type(book)',
-      'topic(Wereldoorlog II)',
-      'pubYearRange(5_OlderThan50)'
-    ],
+    facet: ['topic(Wereldoorlog II)', 'pubYearRange(5_OlderThan50)'],
     page: 1
   })
-  // END USE OF SOURCE
 
-  // START USE OF SOURCE: Laurens
+  // START USE OF SOURCE: Laurens Aarnoudse
   .then(result => JSON.parse(result))
   .then(result => {
     let keys = getData(result)
   })
 
-  .catch(err => console.log(err)) // Something went wrong in the request to the API
+  .catch(err => console.log(err))
+// END USE OF SOURCE: https://github.com/rijkvanzanten/node-oba-api
 
 function getData(data) {
-  // START USE OF SOURCE: Martijn Reeuwijk, Jesse Dijkman & Laurens
-  let dataStore = data.aquabrowser.results.result.map(e => {
+  let apiResult = data.aquabrowser.results.result
+  let dataStore = apiResult.map(e => {
+    // START USE OF SOURCE: Jesse Dijkman
     return {
       title: e.titles
         ? e.titles['short-title']
           ? e.titles['short-title'].$t
           : 'No $t-titel'.toUpperCase()
         : 'No titel'.toUpperCase(),
+
       year: e.publication
         ? e.publication.year
           ? parseInt(e.publication.year.$t, 10)
           : 'No $t-year'.toUpperCase()
         : 'No year'.toUpperCase(),
+
       author: e.authors
         ? e.authors['main-author']
           ? e.authors['main-author'].$t
           : 'No $t-author'.toUpperCase()
         : 'No author'.toUpperCase(),
+
       description: e.summaries
         ? e.summaries.summary
           ? e.summaries.summary.$t
           : 'No $t-description'.toUpperCase()
         : 'No description'.toUpperCase(),
+
       language: e.languages
         ? e.languages.language
           ? e.languages.language.$t
           : 'No $t-language'.toUpperCase()
         : 'No language'.toUpperCase(),
+
       pages: e.description
         ? e.description['physical-description']
           ? parseInt(
@@ -88,8 +82,15 @@ function getData(data) {
           : 0
         : 'No physical description'.toUpperCase()
     }
+    // END USE OF SOURCE: Jesse Dijkman
   })
+  // START USE OF SOURCE: Sterre van Geest
+  let dataStoreString = JSON.stringify(dataStore)
+
+  fs.appendFile('data/data.json', dataStoreString, err => {
+    if (err) throw err
+  })
+  // END USE OF SOURCE: Sterre van Geest
   console.log(dataStore)
-  // END USE OF SOURCE
+  // END USE OF SOURCE: Laurens Aarnoudse
 }
-// END USE OF SOURCE
